f4e74ec119f11e1a16d6c70af4d6105110c83fc8
Ummehani <uhani2683@gmail.com>
2026-03-13 19:49:03 +0530

    statefile locking

---
+++ Day-6-lockfile/main.tf
@@ -0,0 +1,16 @@
+resource "aws_vpc" "name" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "test-vpc"
+    }
+  
+}
+
+resource "aws_subnet" "name" {
+    vpc_id = aws_vpc.name.id
+    cidr_block = "10.0.0.0/24"
+    tags = {
+        Name = "test-subnet"
+    }
+  
+}
